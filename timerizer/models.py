6a3417ca438b2dd6130a5453943e3a73cd4c458a
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-12-01 13:43:11 -0300

    Update models.py

--- timerizer/models.py
+++ timerizer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class ImportantDate(models.Model):
+    year = models.IntegerField()
+    week_day = models.IntegerField()
+    date_number = models.IntegerField()
+    month = models.CharField()
+    bisiesto = models.BooleanField()
+    
+    def __str__(self):
+        return self.year
+    
+my_month_b = ImportantDate.objects.all()
+coincidens = my_month_b.filter(month = Agost, day = 9)
